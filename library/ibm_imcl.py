38c2564fcbe181153cfa45d825b57ea2398a3e8f
tdavison784 <tntdavison@gmail.com>
2019-02-14 10:15:40 -0600

    adding latest into proper structure

--- library/ibm_imcl.py
+++ library/ibm_imcl.py
@@ -15,7 +15,7 @@ module: ibm_imcl
 
 short_description: Module that takes care of installing packages via imcl cli.
 
-version_added: "2.1"
+version_added: "2.3"
 
 description:
     - Module that takes care of installing IBM products via imcl cli.
@@ -44,6 +44,12 @@ options:
         description:
             - Path that leads to imcl tool for installing packages.
         required: true
+    properties:
+        description:
+            - Additonal properties to use during product install.
+            - For example, in order to install IHS you will need to use
+            - use user.ihs.allowNonRootSilentInstall=true,user.ihs.httpPort=8080
+        required: false
     name:
         description:
             - Name of package to be installed, updated, or removed from any given cell.
@@ -78,6 +84,19 @@ EXAMPLES = '''
     state: update
     path: /opt/IBM/InstallationManager/eclipse/tools/imcl
     name: com.ibm.websphere.ND.v85_8.5.5013.20180112_1418
+- name: INSTALL IBM IHS WITH PROPERTIES
+  ibm_imcl:
+    state: present
+    path: /opt/IBM/InstallationManager/eclipse/tools/imcl
+    dest: /opt/IBM/WebSphere/HTTPServer
+    name: com.ibm.websphere.IHS.v85_8.5.5000.20130514_1044
+    src: /tmp/IHS-Binaries/
+    properties: user.ihs.allowNonRootSilentInstall=true,user.ihs.httpPort=8080
+- name: ROLLBACK LATEST FIXPACK
+  ibm_imcl:
+    state: rollback
+    name: com.ibm.websphere.ND.v85_8.5.5013.20180112_1418
+    path: /opt/IBM/InstallationManager/eclipse/tools/imcl
 '''
 
 RETURN = '''
@@ -99,95 +118,192 @@ message:
 '''
 
 
-def install_package(module,path,src,shared_resource,dest,name,properties):
+def install_package_local(module):
     """Function that takes care of installing new packages into the target environment."""
 
-    if properties is None:
-        package_install =  module.run_command(path + " -acceptLicense -repositories " + src +
-                                          " -installationDirectory " + dest + " -log /tmp/IBM-Install.log " +
-                                          "-sharedResourcesDirectory " + shared_resource + " install " + name,
-                                          use_unsafe_shell=True)
-    if properties is not None:
-        package_install =  module.run_command(path + " -acceptLicense -repositories " + src +
-                                          " -installationDirectory " + dest + " -log /tmp/IBM-Install.log " +
-                                          "-sharedResourcesDirectory " + shared_resource + " install " + name +
-                                          " -properties " + properties,
-                                          use_unsafe_shell=True)
-    if package_install[0] != 0:
+    if module.params['properties'] is None:
+        lpackage_install_cmd = """{0} -acceptLicense -repositories {1} \
+                -installationDirectory {2} -log /tmp/IBM-Install.log \
+                -sharedResourcesDirectory {3} install {4}""".format(module.params['path'],
+                        module.params['src'], module.params['dest'], module.params['shared_resource'],
+                        module.params['name'])
+        lpackage_install = module.run_command(lpackage_install_cmd, use_unsafe_shell=True)
+
+    if module.params['properties'] is not None:
+        lpackage_install_cmd = """{0} -acceptLicense -repositories {1} \
+                -installationDirectory {2} -log /tmp/IBM-Install.log \
+                -sharedResourcesDirectory {3} install {4} -properties {5}""".format(module.params['path'],
+                        module.params['src'], module.params['dest'], module.params['shared_resource'],
+                        module.params['name'], module.params['properties'])
+        lpackage_install = module.run_command(lpackage_install_cmd, use_unsafe_shell=True)
+
+    if lpackage_install[0] != 0:
         module.fail_json(
-            msg="Failed to install package: %s. Please see log in /tmp for more details." % (name),
+            msg="Failed to install package: {0}. Please see log in /tmp for more details.".format(module.params['name']),
             changed=False,
-            stderr=package_install[2]
+            stderr=lpackage_install[2]
         )
 
     module.exit_json(
-        msg="Succesfully installed package: %s to location: %s. For installation details please see log in /tmp/. "
-            % (name, dest),
+        msg="Succesfully installed package: {0}to location: {1}. For installation details please see log in /tmp/. ".format(module.params['name'],
+            module.params['dest']),
         changed=True
     )
 
 
-def update_package(module,path,src,shared_resource,name):
+def install_package_remote(module):
+    """
+    Function that will install packages
+    from a remote ibm repo
+    """
+
+    if module.params['properties'] is None:
+        rpackage_install_cmd = """{0}/ -repositories {1} -installationDirectory {2} \
+                -log /tmp/IBM_install.log -sharedResourcesDirectory {3} \
+                install {4} -secureStorageFile {5} -masterPasswordFile {6} \
+                -acceptLicense""".format(module.params['path'],module.params['src'],
+                        module.params['dest'],module.params['shared_resources'],
+                        module.params['name'],module.params['secure_storage'],
+                        module.params['passwor_file'])
+        rpackage_install = module.run_command(rpackage_install_cmd, use_unsafe_shell=True)
+
+    if module.params['properties'] is not None:
+        rpackage_install_cmd = """{0}/ -repositories {1} -installationDirectory {2} \
+                -log /tmp/IBM_install.log -sharedResourcesDirectory {3} \
+                install {4} -secureStorageFile {5} -masterPasswordFile {6} \
+                -acceptLicense -properties {7}""".format(module.params['path'],module.params['src'],
+                        module.params['dest'],module.params['shared_resources'],
+                        module.params['name'],module.params['secure_storage'],
+                        module.params['password_file'],module.params['properties'])
+        rpackage_install = module.run_command(rpackage_install_cmd, use_unsafe_shell=True)
+
+    if rpackage_install[0] != 0:
+        module.fail_json(
+            msg="Failed to install package(s) {0}".format(module.params['name']),
+            changed=False,
+            stderr=rpackage_install[2],
+            stdout=rpackage_install[1]
+        )
+    module.exit_json(
+        msg="Successfully installed package(s) {0}".format(module.params['name']),
+        changed=True
+    )
+
+
+def update_package_local(module):
+    """Function that updates packages for target environment."""
+    
+
+    lpackage_update_cmd = """{0} -acceptLicense -sharedResourcesDirectory {1} \
+install {2} -repositories {3} -log /tmp/IBM-Update.log""".format(module.params['path'],
+                    module.params['shared_resource'], module.params['name'], module.params['src'])
+
+    lpackage_update = module.run_command(lpackage_update_cmd, use_unsafe_shell=True)
+    if lpackage_update[0] != 0:
+        module.fail_json(
+            msg="Failed to update package: {0}. Please see log in /tmp/ for more details.".format(module.params['name']),
+            changed=False,
+            details=lpackage_update_cmd,
+            stderr=lpackage_update[2]
+        )
+    module.exit_json(
+        msg="Succesfully updated package: {0}".format(module.params['name']),
+        changed=True
+    )
+
+def update_package_remote(module):
     """Function that updates packages for target environment."""
 
-    package_update = module.run_command(path + " -acceptLicense -sharedResourcesDirectory " + shared_resource
-                                        + " install " + name + " -repositories " + src +
-                                        " -log /tmp/IBM-Install.log", use_unsafe_shell=True)
-    if package_update[0] != 0:
+    rpackage_update_cmd = """{0} -acceptLicense -sharedResourcesDirectory {1} \
+            install {2} -repositories {3} -log /tmp/IBM-Update.log \
+            -secureStorageFile {4} -masterPasswordFile {5}""".format(module.params['path'],
+                    module.params['shared_resource'], module.params['name'], module.params['src'],
+                    module.params['secure_storage'], module.params['password_file'])
+
+    rpackage_update = module.run_command(rpackage_update_cmd, use_unsafe_shell=True)
+
+    if rpackage_update[0] != 0:
         module.fail_json(
-            msg="Failed to update package: %s. Please see log in /tmp/ for more details." % (name),
+            msg="Failed to update package: {0}. Please see log in /tmp/ for more details.".format(module.params['name']),
             changed=False,
-            stderr=package_update[2]
+            stderr=rpackage_update[2]
         )
     module.exit_json(
-        msg="Succesfully updated package: %s" % (name),
+        msg="Succesfully updated package: {0}".format(module.params['name']),
         changed=True
     )
 
-
-def rollback_package(module,path,name):
+def rollback_package(module):
     """Function to rollback to a previous package version."""
 
-    rllbck_pckg = module.run_command(path+" rollback "+name,use_unsafe_shell=True)
+    rllbck_pckg_cmd = """{0} rollback {1}""".format(module.params['path'],
+            module.params['name'])
+    rllbck_pckg = module.run_command(rllbck_pckg_cmd, use_unsafe_shell=True)
 
     if rllbck_pckg[0] != 0:
         module.fail_json(
-            msg="Failed to rollback package: %s because the package was not previously installed" %(name),
+            msg="Failed to rollback package: {0} because the package was not previously installed".format(module.params['name']),
             changed=False,
             stderr=rllbck_pckg[2]
         )
     module.exit_json(
-        msg="Successfully rolled back package: %s" %(name),
+        msg="Successfully rolled back package: {0}".format(module.params['name']),
         changed=True
     )
 
 
-def uninstall_package(module,path,name):
-    """Functiont that will remove any given package from target environment."""
+def uninstall(module):
+    """ Function that will uninstall all a package
+    or if all: yes is specified will uninstall all
+    packages in the given WAS cell
+    """
 
-    remove_package = module.run_command(path + " uninstall " + name
-                                        + " -log /tmp/IBM-UnInstall.log", use_unsafe_shell=True)
-
-    if remove_package[0] != 0:
-        module.fail_json(
-            msg="Failed to remove package: %s from cell. See log in /tmp/ for more details." % (name),
-            changed=False,
-            stderr=remove_package[2]
+    if (module.params['remove_all'] == 'no'):
+        uninstall_cmd = """{0} uninstall {1}""".format(module.params['path'],
+            module.params['name'])
+        uninstall = module.run_command(uninstall_cmd, use_unsafe_shell=True)
+    
+        if uninstall[0] != 0:
+            module.fail_json(
+                msg="Failed to uninstall package {0}".format(module.params['name']),
+                changed=False,
+                stderr=uninstall[2]
+            )
+        module.exit_json(
+                msg="Succesfully uninstalled package {0}".format(module.params['name']),
+                changed=True
         )
-    module.exit_json(
-        msg="Succesfully removed package: %s from cell." % (name),
-        changed=True
-    )
+
+    if (module.params['remove_all'] == 'yes'):
+        uninstallAll_cmd = """{0} uninstallAll""".format(module.params['path'])
+
+        uninstallAll = module.run_command(uninstallAll_cmd, use_unsafe_shell=True)
+
+        if uninstallAll[0] != 0:
+            module.fail_json(
+                msg="Failed to uninstall all products. See stderr/stdout for details...",
+                changed=False,
+                stderr=uninstallAll[2],
+                stdout=uninstallAll[1]
+            )
+
+        module.exit_json(
+            msg="Succesfully removed all packages",
+            changed=True
+        )
 
 
-def package_check(module,path,name):
+def package_check(module):
     """Function that will be checking target cell for package existance.
     This portion will be doing package lookups to ensure that the package being installed
     either exists in the cell, or doesn't for all module.params['state']
     """
 
-    check_package = module.run_command(path + " listInstalledPackages", use_unsafe_shell=True)
- 
+    check_package_cmd = """{0} listInstalledPackages""".format(module.params['path'])
+    check_package = module.run_command(check_package_cmd, use_unsafe_shell=True)
+
+    name = module.params['name']
+
     name = name.split()
     for package in name:
       if package in check_package[1]:
@@ -204,12 +320,15 @@ def main():
 
     module = AnsibleModule(
         argument_spec=dict(
+            remove_all=dict(type='str', required=False, choices=['yes', 'no'], default='no'),
             state=dict(type='str', required=True, choices=['present', 'absent', 'update', 'rollback']),
-            src=dict(type='str', required=True),
+            src=dict(type='str', required=False),
             dest=dict(type='str', required=False),
             path=dict(type='str', required=True),
             name=dict(type='str', required=False),
             shared_resource=dict(type='str', required=False),
+            secure_storage=dict(type='str', required=False, default=None),
+            password_file=dict(type='str', required=False, default=None),
             properties=dict(type='str', required=False, default=None)
         ),
         supports_check_mode = True,
@@ -218,44 +337,58 @@ def main():
              ["state","update", ["dest", "shared_resource"]]]]
     )
 
+    remove_all = module.params['remove_all']
     state = module.params['state']
     src = module.params['src']
     dest = module.params['dest']
     path = module.params['path']
     name = module.params['name']
     shared_resource = module.params['shared_resource']
+    secure_storage = module.params['secure_storage']
+    password_file =  module.params['password_file']
     properties = module.params['properties']
-    pckg_check = package_check(module,path,name)
+
+    if remove_all == 'yes':
+        uninstall(module)
+
+    pckg_check = package_check(module)
 
 
     if pckg_check != 1:
-        if state == 'present' and not module.check_mode:
-            install_package(module,path,src,shared_resource,dest,name,properties)
-        if state == 'update' and not module.check_mode:
-            update_package(module,path,src,shared_resource,name)
-        if state == 'rollback':
-            rollback_package(module,path,name)
+        if (state == 'present') and not (module.check_mode):
+            install_package_local(module)
+        if (state == 'present') and (secure_storage is not None) and not (module.check_mode):
+            install_package_remote(module)
+        if (state == 'update') and (secure_storage is None) and not (module.check_mode):
+            update_package_local(module)
+        if (state == 'update') and (secure_storage is not None) and not (module.check_mode):
+            update_package_remote(module)
+        if (state == 'rollback') and not (module.check_mode):
+            rollback_package(module)
         if module.check_mode:
             if state == 'present':
-                module.exit_json(msg="Package: %s will be installed to location %s" % (name,dest),change=True)
+                module.exit_json(msg="Package: {0} will be installed to location {1}".format(name,dest),change=True)
             if state == 'update':
-                module.exit_json(msg="Package: %s will be updated" % (name),changed=True)
+                module.exit_json(msg="Package: {0} will be updated".format(name),changed=True)
             if state == 'absent':
-                module.exit_json(msg="Package: %s is not present in cell. Nothing to remove." % (name),changed=False)
-            if state == 'rollback':
-                module.exit_json(msg="Package %s is not present in cell. Nothing to remove" % (name),changed=False)
+                module.exit_json(msg="Package: {0} is not present in cell. Nothing to remove.".format(name),changed=False)
+            if (state == 'rollback'):
+                module.exit_json(msg="Package {0} is not present to rollback".format(name),changed=False)
     elif pckg_check != 0:
-        if state == 'present':
-            module.exit_json(msg="Package %s is already present." % (name),changed=False)
-        if state == 'absent' and not module.check_mode:
-            uninstall_package(module,path,name)
-        if state == 'rollback' and not module.check_mode:
-            rollback_package(module,path,name)
+        if (state == 'present'):
+            module.exit_json(msg="Package {0} is already present.".format(name),changed=False)
+        if (state == 'absent') and not (module.check_mode):
+            uninstall_package(module)
+        if (state == 'update') and not (module.check_mode):
+            module.exit_json(msg="Package {0} is already present.".format(name),changed=False)
+        if (state == 'rollback') and not (module.check_mode):
+            module.exit_json(msg="Package {0} not is not present to rollback.".format(name), changed=False)
         if module.check_mode:
-            if state == 'absent':
-                module.exit_json(msg="Package %s will be removed." % (name),changed=True)
-            if state == 'rollback':
-                module.exit_json(msg="Package %s will be rolled back" %(name),changed=True)
+            if (state == 'absent'):
+                module.exit_json(msg="Package {0} will be removed.".format(name),changed=True)
+            if (state == 'rollback'):
+                module.exit_json(msg="Package {0} will be rolled back".format(name),changed=True)
+
 
 if __name__ == '__main__':
     main()
